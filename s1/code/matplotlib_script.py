3d11c1bbd15930f924a1c7917539704285ec8a08
Toki Migimatsu <toki.migimatsu@gmail.com>
2019-09-29 16:45:54 -0700

    Fix matplotlib script

--- s1/code/matplotlib_script.py
+++ s1/code/matplotlib_script.py
@@ -4,21 +4,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = range(100)
+x = np.arange(100) / 5
 y = np.sin(x)
 
 plt.plot(x,y)
-plt.title("sin(x)")
+plt.title("Line plot")
 plt.xlabel("x")
-plt.xlabel("y")
+plt.ylabel("sin(x)")
 plt.show()
 
 plt.scatter(x,y)
+plt.title("Scatter plot")
+plt.xlabel("x")
+plt.ylabel("sin(x)")
 plt.show()
 
 #Adapted from https://matplotlib.org/tutorials/introductory/sample_plots.html
 data = np.random.randn(2, 100)
 plt.hist(data[0])
+plt.title("Histogram")
+plt.xlabel("data[0]")
+plt.ylabel("Count")
 plt.show()
+
 plt.hist2d(data[0], data[1])
+plt.title("2D Histogram")
+plt.xlabel("data[0]")
+plt.ylabel("data[1]")
 plt.show()
